a45f0fe04f0c0b057689b432a4af2bf96439119f
Pedro-Ponteiro <silv9797@gmail.com>
2023-04-22 00:01:58 -0300

    feat: add cache_credit_filepath

--- PersonalFinanceControl/LocalManagementConstants.py
+++ PersonalFinanceControl/LocalManagementConstants.py
@@ -4,4 +4,5 @@ script_location = os.path.dirname(os.path.abspath(__file__))
 
 CACHE_FOLDER = os.path.join(script_location, "cache")
 CACHE_BILL_FILEPATH = os.path.join(CACHE_FOLDER, "open_bill_details.pickle")
+CACHE_CREDIT_FILEPATH = os.path.join(CACHE_FOLDER, "credit_limit.pickle")
 SECRETS_LOCATION = os.path.join(script_location, "secrets.prod.json")
